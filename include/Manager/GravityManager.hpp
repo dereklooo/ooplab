c36ed631df818dfac6aa368ae739e4b075a45133
Benson <tt23912681@gmail.com>
2025-04-07 19:54:13 +0800

    add

---
+++ include/Manager/GravityManager.hpp
@@ -0,0 +1,8 @@
+//
+// Created by benson on 2025/4/3.
+//
+
+#ifndef GRAVITYMANAGER_HPP
+#define GRAVITYMANAGER_HPP
+
+#endif //GRAVITYMANAGER_HPP
---
+++ include/Object/Object.hpp
@@ -0,0 +1,8 @@
+//
+// Created by benson on 2025/4/3.
+//
+
+#ifndef OBJECT_HPP
+#define OBJECT_HPP
+
+#endif //OBJECT_HPP
