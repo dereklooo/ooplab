9409328a65743579b4b3649a49044c60f3e2c7e5
Benson <tt23912681@gmail.com>
2025-04-08 22:17:57 +0800

    add

--- include/Manager/GravityManager.hpp
+++ include/Manager/GravityManager.hpp
@@ -7,22 +7,18 @@
 #include "Util/Time.hpp"
 #include "Mario/Mario.hpp"
 #include "Monsters/Monster.hpp"
-
 class GravityManager {
     public:
         GravityManager(const std::shared_ptr<Mario> &mario,
             std::vector<std::shared_ptr<Monster>> &monsters,
             const std::vector<std::shared_ptr<SceneObject>>& Scenes) {
-
-            GravityObject.push_back(mario);
-            for (const auto &monster : monsters) {
-                GravityObject.push_back(monster);
-            }
+            this->Mario = mario;
+            this->Monsters = monsters;
             this->Scenes = Scenes;
         }
         void Update(){
             Util::Time::Update();
-            for(auto &object : GravityObject) {
+            for(auto &object : Monsters) {
                 if(IsFalling(object)) {
                     object->SetGravity(object->GetGravity() + gravity * Util::Time::GetDeltaTimeMs() / 1000);
                     object->SetFalling(true);
@@ -33,6 +29,15 @@ class GravityManager {
                 }
                 object->SetPosition({object->GetPosition().x,object->GetPosition().y - object->GetGravity()});
             }
+            if(IsFalling(Mario)) {
+                Mario->SetGravity(Mario->GetGravity() + gravity * Util::Time::GetDeltaTimeMs() / 1000);
+                Mario->SetFalling(true);
+            }
+            else {
+                Mario->SetGravity(0.0f);
+                Mario->SetFalling(false);
+            }
+            Mario->SetPosition({Mario->GetPosition().x,Mario->GetPosition().y - Mario->GetGravity()});
         };
         bool IsFalling(const std::shared_ptr<Object> &Object) {
             for(auto &Scene : Scenes) {
@@ -45,8 +50,10 @@ class GravityManager {
     private:
         const float gravity = 9.8f;
         std::shared_ptr<Util::Time> Time = std::make_shared<Util::Time>();
-        std::vector<std::shared_ptr<Object>> GravityObject;
+        std::vector<std::shared_ptr<Monster>> Monsters;
         std::vector<std::shared_ptr<SceneObject>> Scenes;
+        std::shared_ptr<Mario> Mario;
+        std::vector<std::shared_ptr<Object>> GravityObject;
 };
 #endif
 //GRAVITYMANAGER_HPP
